677e1f1ba055208999a83270ba88ce4b60395aad
SandeshGC <gcsandesh01@gmail.com>
2024-03-22 22:08:28 +0545

    create first two sections

---
+++ assets/js/persona.js
@@ -0,0 +1,3 @@
+import persona from "../data/persona.json" assert { type: "json" }
+
+console.log("persona", persona)
---
+++ assets/js/site.js
@@ -0,0 +1,3 @@
+import site from "../data/site.json" assert { type: "json" }
+
+console.log("site", site)
