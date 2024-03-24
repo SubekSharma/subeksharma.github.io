8d16940c1c8c2bde164098f8c64b601a6d90de8b
SandeshGC <gcsandesh01@gmail.com>
2024-03-24 22:05:17 +0545

    fix: logos in education section, other minor styles changed

--- assets/js/education.js
+++ assets/js/education.js
@@ -8,37 +8,35 @@ const bgIconUrl = "assets/images/purple_circle_filled.svg"
 const educationListEl = document.getElementById("educationList")
 
 education.map((item) => {
+  //   <h3 class="text-2xl text-serif text-center">
+  //   ${item.degree}
+  //  </h3>
 
-
-//   <h3 class="text-2xl text-serif text-center">
-//   ${item.degree}
-//  </h3>
-
-//  <div>
-//  <hr class="border rounded-full w-full border-indigo-500"/>
-//  <p class="font-semibold text-sm text-center">
-//  ${item.institution}
-//  </p>
-//  
-//  </div>
+  //  <div>
+  //  <hr class="border rounded-full w-full border-indigo-500"/>
+  //  <p class="font-semibold text-sm text-center">
+  //  ${item.institution}
+  //  </p>
+  //
+  //  </div>
   const educationItem = `
- <div class="flex flex-col h-[400px] border border-red-500 w-full  py-4">
-    <div class="flex items-center justify-center">
+ <div class="flex flex-col h-[400px] w-full  py-4">
+    <div class="relative flex items-center justify-center">
       <img
-     src=${bgIconUrl}
-     alt=${bgIconAlt}
-     class="h-24 w-24"
-        />
-   <img
-     src=${item.iconUrl}
-     alt=${item.iconAlt}
-     class="absolute left-[50%] top-[50%] transform translate-x-[-50%] translate-y-[-50%] w-10 h-10"
-   />
+        src=${bgIconUrl}
+        alt=${bgIconAlt}
+        class="h-24 w-24"
+      />
+      <img
+        src=${item.iconUrl}
+        alt=${item.iconAlt}
+        class="absolute left-[50%] top-[50%] transform translate-x-[-50%] translate-y-[-50%] w-10 h-10 z-50"
+      />
 
    </div>
 
-     <div class="text-2xl flex-1 border-b border-[#5e17eb] text-serif flex items-center justify-center text-center">
-       <h3>
+     <div class="text-2xl flex-1 border-b-2 border-[#5e17eb] text-serif flex items-center justify-center text-center">
+       <h3  class="text-serif">
 
       ${item.degree}
       </h3>
@@ -51,15 +49,11 @@ education.map((item) => {
        <p class="text-sm text-center">${item.year}</p>
      </div>
 
- </div>
-
-
-
- 
+ </div> 
  `
 
   const containerEl = document.createElement("div")
-  containerEl.className =""
+  containerEl.className = ""
 
   containerEl.innerHTML = educationItem
 
