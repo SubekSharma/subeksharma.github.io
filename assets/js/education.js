65731b265c86061fb4a6e6dfbfcf8ab6491f240d
subeksharma <subeksharmaofficial@gmail.com>
2024-03-24 20:55:40 +0545

    some changes

--- assets/js/education.js
+++ assets/js/education.js
@@ -8,33 +8,58 @@ const bgIconUrl = "assets/images/purple_circle_filled.svg"
 const educationListEl = document.getElementById("educationList")
 
 education.map((item) => {
+
+
+//   <h3 class="text-2xl text-serif text-center">
+//   ${item.degree}
+//  </h3>
+
+//  <div>
+//  <hr class="border rounded-full w-full border-indigo-500"/>
+//  <p class="font-semibold text-sm text-center">
+//  ${item.institution}
+//  </p>
+//  
+//  </div>
   const educationItem = `
- <div class="relative mx-auto">
-   <img
+ <div class="flex flex-col h-[400px] border border-red-500 w-full  py-4">
+    <div class="flex items-center justify-center">
+      <img
      src=${bgIconUrl}
      alt=${bgIconAlt}
      class="h-24 w-24"
-   />
+        />
    <img
      src=${item.iconUrl}
      alt=${item.iconAlt}
      class="absolute left-[50%] top-[50%] transform translate-x-[-50%] translate-y-[-50%] w-10 h-10"
    />
+
+   </div>
+
+     <div class="text-2xl flex-1 border-b border-[#5e17eb] text-serif flex items-center justify-center text-center">
+       <h3>
+
+      ${item.degree}
+      </h3>
+     </div>
+
+    <div class="font-semibold h-[100px] flex flex-col  text-sm text-center">
+      <p class="flex-1 flex items-center justify-center">
+       ${item.institution}
+     </p>
+       <p class="text-sm text-center">${item.year}</p>
+     </div>
+
  </div>
 
- <h3 class="text-2xl text-serif text-center">
-  ${item.degree}
- </h3>
 
- <div class="border rounded-full w-full border-indigo-500"></div>
- <p class="font-semibold text-sm text-center">
-    ${item.institution}
- </p>
- <p class="text-sm text-center">Nov 2019 - Apr 2024</p>
-</div>`
+
+ 
+ `
 
   const containerEl = document.createElement("div")
-  containerEl.className = "grid grid-cols-1 items-center gap-4"
+  containerEl.className =""
 
   containerEl.innerHTML = educationItem
 
